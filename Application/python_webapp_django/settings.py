df808d71bc6d9e4bf30e9b7d34bcbe3d3a318fa9
godetsophie <92625194+godetsophie@users.noreply.github.com>
2022-01-22 21:59:07 +0100

    21:58

--- Application/python_webapp_django/settings.py
+++ Application/python_webapp_django/settings.py
@@ -87,14 +87,14 @@ WSGI_APPLICATION = 'python_webapp_django.wsgi.application'
 #         }
 #     }
 # else:
-hostname = os.environ['DBHOST']
+# hostname = os.environ['DBHOST']
 DATABASES= {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DBNAME'],
-        'HOST': hostname + ".postgres.database.azure.com",
-        'USER': os.environ['DBUSER'] + "@" + hostname,
-        'PASSWORD': os.environ['DBPASS']  
+        'NAME': 'jiggyjigsaw',
+        'HOST': 'sophiegodet.postgres.database.azure.com',
+        'USER': 'admingsg4@sophiegodet',
+        'PASSWORD': '25Decembre'  
     }
 }
 
