9838891dddb522696c68dfdeea224c46ce494858
godetsophie <92625194+godetsophie@users.noreply.github.com>
2022-01-22 23:07:44 +0100

    23:07

--- Application/python_webapp_django/production.py
+++ Application/python_webapp_django/production.py
@@ -1,7 +1,5 @@
 import os
 from pathlib import Path
-from django.db import DatabaseError
-from numpy import true_divide
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
