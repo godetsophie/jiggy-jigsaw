f2964c0af3ea6a0c277cc569411ce210f22f81f8
godetsophie <92625194+godetsophie@users.noreply.github.com>
2022-01-22 14:17:41 +0100

    14:17

--- Tests/unit_tests/test_views.py
+++ Tests/unit_tests/test_views.py
@@ -36,9 +36,9 @@ class ViewTest(TestCase):
     def test_unit_contact(self):
         """Tests the contact page."""
         response = self.client.get('/contact')
-        self.assertContains(response, 'Contact', 3, 200)
+        self.assertContains(response, 'Contact', 2, 200)
 
     def test_unit_about(self):
         """Tests the about page."""
         response = self.client.get('/about')
-        self.assertContains(response, 'About', 3, 200)
+        self.assertContains(response, 'About', 2, 200)
