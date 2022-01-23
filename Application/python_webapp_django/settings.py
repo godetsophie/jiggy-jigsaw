e555deaa7557e8ba2ce24002a978b80e13498dc2
godetsophie <92625194+godetsophie@users.noreply.github.com>
2022-01-23 01:02:00 +0100

    1:01

--- Application/python_webapp_django/settings.py
+++ Application/python_webapp_django/settings.py
@@ -77,9 +77,16 @@ WSGI_APPLICATION = 'python_webapp_django.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jiggyjigsaw',
+        'HOST': 'sophiegodet.postgres.database.azure.com',
+        'USER': 'adminsg4@sophiegodet',
+        'PASSWORD':'25Decembre' 
     }
 }
 
